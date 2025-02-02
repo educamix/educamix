38a4b6a161396e6d6e4589171e55d5f78763156a
lucasdav <lucassouzadavanso@gmail.com>
2025-02-02 12:05:26 -0300

    finalizacao tela quiz - TO DO - integrar api

--- src/pages/Quiz/index.tsx
+++ src/pages/Quiz/index.tsx
@@ -4,6 +4,7 @@ import 'tailwindcss/tailwind.css'
 import React, { useState } from 'react'
 
 export default function Quiz() {
+  const [message, setMessage] = useState('')
   const navigate = useNavigate()
 
   const quiz = [
@@ -18,78 +19,47 @@ export default function Quiz() {
 
   const handleQuizSubmit = (e: React.FormEvent) => {
     e.preventDefault()
+    const formData = new FormData(e.target as HTMLFormElement)
+    const selectedAnswer = formData.get('quiz') as string
+    setMessage(selectedAnswer === quiz[0].correta
+      ? 'Correto'
+      : 'Errada')
   }
 
   return (
-    <div className={clsx(
-      'flex flex-col items-center pt-20 gap-9 w-11/12 max-w-80 mx-auto',
-      'animate-fade-down ease-in',
-    )}
+    <div
+      className={clsx(
+        'flex flex-col items-center pt-20 gap-9 w-11/12 max-w-80 mx-auto',
+        'animate-fade-down ease-in',
+      )}
     >
-
       <div className="flex flex-col gap-6 w-full">
-        <div className="flex flex-col gap-1">
-          <h1 className={clsx(
-            'text-em-pink font-fredoka text-xl font-medium',
-          )}
-          >
-            {quiz[0].disciplina}
-          </h1>
-          <h2 className={clsx(
-            'text-em-white font-inter text-xs font-bold text-center mt-8',
-          )}
-          >
-            {quiz[0].tema}
-          </h2>
-        </div>
-
-        <form
-          className="w-full flex flex-col gap-9"
-          onSubmit={handleQuizSubmit}
-        >
+        <h1 className="text-em-pink font-fredoka text-xl font-medium">{quiz[0].disciplina}</h1>
+        <h2 className="text-em-white font-inter text-xs font-bold text-center mt-8">{quiz[0].tema}</h2>
+        <form className="w-full flex flex-col gap-9" onSubmit={handleQuizSubmit}>
           <div className="flex flex-col gap-3">
-            <label
-              htmlFor="quiz"
-              className={clsx(
-                'text-em-white font-inter text-xs font-bold',
-              )}
-            >
+            <label htmlFor="quiz" className="text-em-white font-inter text-xs font-bold">
               {quiz[0].pergunta}
             </label>
-            {
-              quiz[0].respostas.map((resposta, index) => (
-                <div key={index} className="flex items-center gap-2">
-                  <input
-                    type="radio"
-                    id={resposta}
-                    name="quiz"
-                    value={resposta}
-                    className="h-4 w-4"
-                  />
-                  <label
-                    htmlFor={resposta}
-                    className={clsx(
-                      'text-em-white font-inter text-xs font-bold',
-                    )}
-                  >
-                    {resposta}
-                  </label>
-                </div>
-              ))
-            }
+            {quiz[0].respostas.map((resposta, index) => (
+              <div key={index} className="flex items-center gap-2">
+                <input type="radio" id={resposta} name="quiz" value={resposta} className="h-4 w-4" />
+                <label htmlFor={resposta} className="text-em-white font-inter text-xs font-bold">
+                  {resposta}
+                </label>
+              </div>
+            ))}
           </div>
           <div className="flex flex-col gap-3">
             <button
               type="submit"
-              className={clsx(
-                'rounded-md bg-em-green h-10 flex justify-center items-center',
-                'w-full font-inter text-xs font-bold text-white',
-              )}
+              className={clsx('rounded-md bg-em-green h-10 flex justify-center items-center', 'w-full font-inter text-xs font-bold text-white')}
             >
               Responder
             </button>
           </div>
         </form>
+        {message && <p className="text-white font-bold">{message}</p>}
       </div>
     </div>
   )
