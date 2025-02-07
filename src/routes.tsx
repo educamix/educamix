0fb82f15afcddde2b72eb793d82f76bf2349c5fa
lucasdav <lucassouzadavanso@gmail.com>
2025-02-07 17:23:16 -0300

    fix remocao router posts

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,7 +4,6 @@ import LoadingComponent from "./components/LoadingComponent";
 import Register from "./pages/Register";
 import Admin from "./pages/Admin";
 import AuthUserProvider from "./context/auth";
-import { PostsProvider } from "./context/Posts/PostsContext";
 import { UsersProvider } from "./context/Users/UsersContext";
 import Quiz from "./pages/Quiz";
 import { Ranking } from "./pages/Ranking";
@@ -17,23 +16,21 @@ export default function AppRouter() {
   return (
     <main>
       <AuthUserProvider>
-        <PostsProvider>
-          <UsersProvider>
-            <Router>
-              <Suspense fallback={<LoadingComponent />}>
-                <Routes>
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/admin" element={<Admin />} />
-                  <Route path="/" element={<Home />} />
-                  <Route path="/quiz" element={<Quiz />} />
-                  <Route path="/ranking" element={<Ranking />} />
-                  <Route path="/summary" element={<Summary />} />
-                </Routes>
-              </Suspense>
-            </Router>
-          </UsersProvider>
-        </PostsProvider>
+        <UsersProvider>
+          <Router>
+            <Suspense fallback={<LoadingComponent />}>
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/quiz" element={<Quiz />} />
+                <Route path="/ranking" element={<Ranking />} />
+                <Route path="/summary" element={<Summary />} />
+              </Routes>
+            </Suspense>
+          </Router>
+        </UsersProvider>
       </AuthUserProvider>
     </main>
   );
