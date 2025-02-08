e7f945a28cf7ed947f9c72caa95d10a52bc186e7
lucasdav <lucassouzadavanso@gmail.com>
2025-02-07 21:45:12 -0300

    inclusao private routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,17 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoadingComponent from "./components/LoadingComponent";
-import Register from "./pages/Register";
-import Admin from "./pages/Admin";
-import AuthUserProvider from "./context/auth";
-import { UsersProvider } from "./context/Users/UsersContext";
-import Quiz from "./pages/Quiz";
-import { Ranking } from "./pages/Ranking";
-import { Summary } from "./pages/Summary";
+import { lazy, Suspense } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import LoadingComponent from './components/LoadingComponent'
+import Register from './pages/Register'
+import Admin from './pages/Admin'
+import AuthUserProvider from './context/auth'
+import { UsersProvider } from './context/Users/UsersContext'
+import Quiz from './pages/Quiz'
+import { Ranking } from './pages/Ranking'
+import { Summary } from './pages/Summary'
+import { PrivateRoute } from './components/PrivateRoute'
 
-const Home = lazy(() => import("./pages/Home"));
-const Login = lazy(() => import("./pages/Login"));
+const Home = lazy(() => import('./pages/Home'))
+const Login = lazy(() => import('./pages/Login'))
 
 export default function AppRouter() {
   return (
@@ -22,16 +23,56 @@ export default function AppRouter() {
               <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/admin" element={<Admin />} />
-                <Route path="/" element={<Home />} />
-                <Route path="/quiz" element={<Quiz />} />
-                <Route path="/ranking" element={<Ranking />} />
-                <Route path="/summary" element={<Summary />} />
+                <Route
+                  path="/admin" element={
+                    <>
+                      <PrivateRoute allowedRoles={['teacher']}>
+                        <Admin />
+                      </PrivateRoute>
+                    </>
+                }
+                />
+                <Route
+                  path="/" element={
+                    <>
+                      <PrivateRoute allowedRoles={['teacher', 'student']}>
+                        <Home />
+                      </PrivateRoute>
+                    </>
+                  }
+                />
+                <Route
+                  path="/quiz" element={
+                    <>
+                      <PrivateRoute allowedRoles={['teacher', 'student']}>
+                        <Quiz />
+                      </PrivateRoute>
+                    </>
+                  }
+                />
+                <Route
+                  path="/ranking" element={
+                    <>
+                      <PrivateRoute allowedRoles={['teacher', 'student']}>
+                        <Ranking />
+                      </PrivateRoute>
+                    </>
+                  }
+                />
+                <Route
+                  path="/summary" element={
+                    <>
+                      <PrivateRoute allowedRoles={['teacher']}>
+                        <Summary />
+                      </PrivateRoute>
+                    </>
+                  }
+                />
               </Routes>
             </Suspense>
           </Router>
         </UsersProvider>
       </AuthUserProvider>
     </main>
-  );
+  )
 }
